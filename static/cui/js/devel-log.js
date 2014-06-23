f9e426a4db5300d4dc62ff4e843b2fb45c42e495
Paweł Marczewski <pawel@codility.com>
2014-06-23 12:22:45 +0200

    Add a mock version of codility_logger

---
+++ static/cui/js/devel-log.js
@@ -0,0 +1,31 @@
+/* Developer version of Codility's logger. */
+
+var DevelLog = {
+    handle : function(level, type, msg, exception) {
+        var s = "[" + level + "]" + " " + type;
+        if (msg)
+            s += ", " + msg;
+        if (exception)
+            s += " (exception: " + exception + ")";
+        console.log(s);
+    },
+
+    flush: function() {},
+
+    debug : function(type, msg, exception) {
+        this.handle("DEBUG", type, msg, exception);
+    },
+
+    info : function(type, msg, exception) {
+        this.handle("INFO", type, msg, exception);
+    },
+
+    warning : function(type, msg, exception) {
+        this.handle("WARNING", type, msg, exception);
+    },
+
+    error : function(type, msg, exception) {
+        this.handle("ERROR", type, msg, exception);
+        this.flush();
+    }
+};
