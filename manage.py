8a6d2eff933ca78203aa9c75d68fa6cca1fa5c71
Paweł Marczewski <pawel@codility.com>
2014-06-25 14:42:18 +0200

    Fix problem with test server trying to bind to unavailable port

--- manage.py
+++ manage.py
@@ -9,4 +9,6 @@ from django.core.management import execute_manager
 import run_settings
 
 if __name__=='__main__':
+    # Try binding to multiple ports in case the first one is not available.
+    os.environ.setdefault('DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8050-8099')
     execute_manager(run_settings)
