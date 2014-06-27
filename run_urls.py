d2865dc321997895a0902923ed0b74cfbf38babd
Paweł Marczewski <pawel@codility.com>
2014-06-27 10:53:54 +0200

    Remove unnecessary static URLs

--- run_urls.py
+++ run_urls.py
@@ -1,6 +1,5 @@
 
 from django.conf.urls import patterns, url
-from django.conf.urls.static import static
 
 import views, run_settings
 
@@ -9,5 +8,3 @@ urlpatterns = patterns(
     url(r'^test/$', 'cui_test', name='cui_test'),
     url(r'^local/$', 'cui_local', name='cui_local')
 )
-
-urlpatterns += static(run_settings.STATIC_URL)
