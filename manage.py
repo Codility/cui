9f9a953ad90d5746f43388f1a6370ef198229d82
Paweł Marczewski <pawel@codility.com>
2014-10-07 09:43:30 +0200

    CUI: update to Django 1.6

--- manage.py
+++ manage.py
@@ -25,12 +25,11 @@
 
 import os, sys
 
-from django.core.management import execute_manager
+from django.core.management import execute_from_command_line
 
-
-import run_settings
 
 if __name__=='__main__':
     # Try binding to multiple ports in case the first one is not available.
     os.environ.setdefault('DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8050-8099')
-    execute_manager(run_settings)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'run_settings')
+    execute_from_command_line(sys.argv)
