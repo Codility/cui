c844a03d3d1e8bdb41aea957116d4db8f59eab63
Paweł Marczewski <pawel@codility.com>
2015-02-13 11:49:30 +0100

    draw trees using SVG

--- static/cui/js/trees.js
+++ static/cui/js/trees.js
@@ -111,11 +111,63 @@ var Trees = (function() {
 var TreeEditor = function($elt, tree_string) {
     var self = {};
 
+    var svgNS = "http://www.w3.org/2000/svg";
+
     self.init = function() {
         self.$elt = $elt;
         self.tree = Trees.parse_tree(tree_string);
 
-        self.$elt.text(JSON.stringify(self.tree));
+        self.svg = document.createElementNS(svgNS, "svg");
+        self.svg.setAttributeNS(null, 'width', 500);
+        self.svg.setAttributeNS(null, 'height', 500);
+
+        $elt.append(self.svg);
+
+        self.draw_tree(self.tree, 250, 50);
+    };
+
+    self.draw_tree = function(tree, root_x, root_y) {
+        if (tree === null)
+            return;
+
+        if (tree.l !== null) {
+            var left_x = root_x - 50;
+            var left_y = root_y + 50;
+            self.draw_edge(root_x, root_y, left_x, left_y);
+            self.draw_tree(tree.l, left_x, left_y);
+        }
+        if (tree.r !== null) {
+            var right_x = root_x + 50;
+            var right_y = root_y + 50;
+            self.draw_edge(root_x, root_y, right_x, right_y);
+            self.draw_tree(tree.r, right_x, right_y);
+        }
+
+        self.draw_node(tree.x, root_x, root_y);
+   };
+
+    self.draw_node = function(value, x, y) {
+        self.add_element('circle', {cx: x, cy: y, r: 15, stroke: 'black', fill: 'white'});
+        self.add_element('text', {x: x, y: y, style: 'text-anchor: middle; dominant-baseline: central;'}, value);
+    };
+
+    self.draw_edge = function(x1, y1, x2, y2) {
+        self.add_element('line', {x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black'});
+    };
+
+
+    self.add_element = function(name, attributes, content) {
+        var elt = document.createElementNS(svgNS, name);
+
+        for (var key in attributes) {
+            elt.setAttributeNS(null, key, attributes[key]);
+        }
+
+        if (content !== undefined) {
+            elt.appendChild(document.createTextNode(content));
+        }
+
+        self.svg.appendChild(elt);
     };
 
     self.init();
