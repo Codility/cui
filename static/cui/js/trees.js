59fd07602e4538633e9a0eb7e7b541c6791a5985
Paweł Marczewski <pawel@codility.com>
2015-02-13 12:13:54 +0100

    draw trees with proper layout

--- static/cui/js/trees.js
+++ static/cui/js/trees.js
@@ -112,6 +112,11 @@ var TreeEditor = function($elt, tree_string) {
     var self = {};
 
     var svgNS = "http://www.w3.org/2000/svg";
+    var node_width = 30;
+    var null_width = 5;
+    var node_height = 50;
+    var null_height = 25;
+    var null_size = 10;
 
     self.init = function() {
         self.$elt = $elt;
@@ -123,31 +128,54 @@ var TreeEditor = function($elt, tree_string) {
 
         $elt.append(self.svg);
 
-        self.draw_tree(self.tree, 250, 50);
+        var width = self.get_width(self.tree);
+        var x = (500 - width) / 2;
+        var y = 0;
+        self.draw_tree(self.tree, x, y);
     };
 
-    self.draw_tree = function(tree, root_x, root_y) {
+    self.get_width = function(tree) {
         if (tree === null)
-            return;
+            return null_width;
 
-        if (tree.l !== null) {
-            var left_x = root_x - 50;
-            var left_y = root_y + 50;
-            self.draw_edge(root_x, root_y, left_x, left_y);
-            self.draw_tree(tree.l, left_x, left_y);
+        if (tree.width !== undefined)
+            return tree.width;
+
+        var left_width = self.get_width(tree.l);
+        var right_width = self.get_width(tree.r);
+
+        tree.width = left_width + right_width + node_width;
+        return tree.width;
+    };
+
+    self.draw_tree = function(tree, x, y, parent_x) {
+        var root_x, root_y;
+        if (tree === null) {
+            root_x = x + null_width / 2;
+            root_y = y + null_height;
+
+            if (parent_x !== undefined)
+                self.draw_edge(parent_x, y, root_x, root_y);
+
+            self.draw_null(root_x, root_y);
+            return;
         }
-        if (tree.r !== null) {
-            var right_x = root_x + 50;
-            var right_y = root_y + 50;
-            self.draw_edge(root_x, root_y, right_x, right_y);
-            self.draw_tree(tree.r, right_x, right_y);
-        }
+
+        var left_width = self.get_width(tree.l);
+        root_x = x + left_width + node_width / 2;
+        root_y = y + node_height;
+
+        if (parent_x !== undefined)
+            self.draw_edge(parent_x, y, root_x, root_y);
+
+        self.draw_tree(tree.l, x, root_y, root_x);
+        self.draw_tree(tree.r, x + left_width + node_width, root_y, root_x);
 
         self.draw_node(tree.x, root_x, root_y);
    };
 
     self.draw_node = function(value, x, y) {
-        self.add_element('circle', {cx: x, cy: y, r: 15, stroke: 'black', fill: 'white'});
+        self.add_element('circle', {cx: x, cy: y, r: node_width/2, stroke: 'black', fill: 'white'});
         self.add_element('text', {x: x, y: y, style: 'text-anchor: middle; dominant-baseline: central;'}, value);
     };
 
@@ -155,6 +183,9 @@ var TreeEditor = function($elt, tree_string) {
         self.add_element('line', {x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black'});
     };
 
+    self.draw_null = function(x, y) {
+        self.add_element('rect', {x: x-null_size/2, y: y-null_size/2, width: null_size, height: null_size, fill: 'black'});
+    };
 
     self.add_element = function(name, attributes, content) {
         var elt = document.createElementNS(svgNS, name);
